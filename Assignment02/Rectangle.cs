d71f5b01511fda52f719c80e8614c8e507ad5728
Jenish Nakrani <Jnakrani5508@conestogac.on.ca>
2022-10-04 22:52:23 -0400

    completed Rectangle class

--- Assignment02/Rectangle.cs
+++ Assignment02/Rectangle.cs
@@ -8,5 +8,46 @@ namespace Assignment02
 {
     internal class Rectangle
     {
+        private int length;
+        private int width;
+
+        //default constructor
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        //non-default constructor
+        public Rectangle(int rLength, int rWidth)
+        {
+            length = rLength;
+            width = rWidth;
+        }
+        public int GetCurrentLength()
+        {
+            return length;
+        }
+        public int SetNewLength(int rLength)
+        {
+            length = rLength;
+            return length;
+        }
+        public int GetCurrentWidth()
+        {
+            return width;
+        }
+        public int SetNewWidth(int rWidth)
+        {
+            width = rWidth;
+            return width;
+        }
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+        public int GetArea()
+        {
+            return length * width;
+        }
     }
 }
